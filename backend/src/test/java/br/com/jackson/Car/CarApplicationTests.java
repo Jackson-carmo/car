936ab2ef5631fa46e1ff183ebf23b4ce5451f47c
Jackson-carmo <jacksoncruciol157@gmail.com>
2022-09-18 14:15:49 -0300

    projeto criado

---
+++ backend/src/test/java/br/com/jackson/Car/CarApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jackson.Car;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
